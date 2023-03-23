84bb25c23e437ed3feca7f0509c4f3eeede38cae
ashwhi06 <ashe.white06@gmail.com>
2023-03-23 15:55:04 -0400

    changing css using Js

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
+
+let footerText = document.querySelector('.footer')
+footerText.style.color = "green"
+
+let dogNames = document.querySelectorAll('.dog-name')
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+    dogNames[i].style.textAlign = "left"
+    // dogImages[i].style.transform = "rotate(180deg)"
+    dogImages[i].style.transform = "scale(-1, -1)"
+}
